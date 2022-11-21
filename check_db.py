d90b3fb30df60a85aaa259bbd8812bcca4936e09
Steph Lee <tofu@Stephs-Air.fios-router.home>
2022-11-21 15:45:36 -0500

    added function to look for and delete any straggling sessions >30days old

--- check_db.py
+++ check_db.py
@@ -15,24 +15,24 @@ def get_info():
         for row in user:
             print(f"user_id: {row['id']}, user_name: {row['username']}, nickname: {row['nickname']}")
             # print(row['username'])
-            print(row['password'])
+            # print(row['password'])
     saved = conn.execute('SELECT * FROM saves').fetchall()
     if not saved:
         print('No saved searches in the database.')
     else:
         for row in saved:
-            print(row[0])
-            print(row[1])
-            print(row[2])
-            print(row[3])
+            print("savename:", row[0])
+            print("params:", row[1])
+            print("results:", row[2])
+            print("user_id:", row[3])
     session = conn.execute('SELECT * FROM session_table').fetchall()
     if not session:
         print('No sessions in the database.')
     else:
         for row in session:
-            print(row['user_token'])
-            print(row['user_id'])
-            print(row[2])
+            print("user_token:", row['user_token'])
+            print("user_id:", row['user_id'])
+            print("timestamp:", row[2])
     conn.close()
 
 def get_savenames():
@@ -59,10 +59,9 @@ def check_expired_sessions():
     expire = datetime.now() - timedelta(days=set_time)
     res = conn.execute('SELECT age,user_id FROM session_table WHERE age < ?', (expire,)).fetchall()
     if res:
-        print('These sessions are too old and will be deleted')
+        print('These sessions are too old and will be deleted:')
         for row in res:
-            print(row['age'])
-            print('user_id:', row['user_id'])
+            print(row['age'], 'user_id:', row['user_id'])
     else:
         print('No expired sessions.')
     conn.execute('DELETE FROM session_table WHERE age < ?', (expire,))
