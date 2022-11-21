0683047b4538d2202cf8552de11e945ee2788c23
Steph Lee <tofu@Stephs-Air.fios-router.home>
2022-11-21 17:37:39 -0500

    reset gitignore

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from app.sneaky import secret
+from flask_session import Session
+from flask_wtf.csrf import CSRFProtect
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = secret
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+# app.config["SESSION_COOKIE_SECURE"] = True
+
+Session(app)
+csrf = CSRFProtect(app)
+
+from app import routes
---
+++ schema.sql
@@ -0,0 +1,32 @@
+DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS saves;
+DROP TABLE IF EXISTS session_table;
+
+CREATE TABLE users(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL UNIQUE,
+    nickname TEXT,
+    password TEXT NOT NULL
+);
+
+CREATE TABLE saves(
+   savename TEXT NOT NULL,
+   params TEXT NOT NULL,
+   results TEXT,
+   user_id INTEGER NOT NULL,
+   PRIMARY KEY (savename, user_id),
+   FOREIGN KEY (user_id) REFERENCES users(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+);
+
+CREATE TABLE session_table(
+    user_token TEXT UNIQUE PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    age TIMESTAMP,
+        -- ON CONFLICT REPLACE,
+        -- if you replace it will log out their other sessions on other devices/browsers
+    FOREIGN KEY (user_id) REFERENCES users(id)
+        ON UPDATE CASCADE
+        ON DELETE CASCADE
+)
