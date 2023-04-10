f7557ebc6c98a4b295663b04a68649341733042d
Steph Lee <stsmlee@gmail.com>
2023-04-10 11:17:54 -0400

    session cookie secure set to true

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = secret
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
-# app.config["SESSION_COOKIE_SECURE"] = True
+app.config["SESSION_COOKIE_SECURE"] = True
 
 # Session(app)
 csrf = CSRFProtect(app)
